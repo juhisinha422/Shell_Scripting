e6f359bd546c51f2b5147dd612e96331bf3d13eb
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2025-05-09 12:28:26 +0530

    Update jetha_lal_ki_duniya.sh

--- day_02/jetha_lal_ki_duniya.sh
+++ day_02/jetha_lal_ki_duniya.sh
@@ -21,3 +21,7 @@ echo "You entered $username"
 
 # arguments
 echo "The characters in $0 are:  $1 $2"
+
+#=====================================================
+#execute like below: bash jetha_lal_ki_duniya.sh iyer bhide
+#output will be: The characters in jetha_lal_ki_duniya.sh are:  iyer bhide
