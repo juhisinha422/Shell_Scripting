4ea04ed9b69bef16300e659ff8c9e5965c93c401
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2025-05-09 12:37:05 +0530

    Create if_else.sh

---
+++ day_02/if_else.sh
@@ -0,0 +1,11 @@
+<< disclaimer
+This is just for infotainment purpose
+disclaimer
+
+read -p "Jetha ne mud ke kise dekha: " bandi
+if [[ $bandi == "daya bhabhi" ]];
+then
+  echo "Jetha is loyal"
+else
+  echo "Jetha is not loyal"
+fi
