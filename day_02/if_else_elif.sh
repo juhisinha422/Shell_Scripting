61e5e17d963258a349b270d5301895f2bb2852b9
Juhi Sinha <46685880+juhisinha422@users.noreply.github.com>
2025-05-09 13:09:20 +0530

    Create if_else_elif.sh

---
+++ day_02/if_else_elif.sh
@@ -0,0 +1,14 @@
+<< disclaimer
+This is just for infotainment purpose
+disclaimer
+
+read -p "Jetha ne mud ke kise dekha: " bandi
+read -p " Jetha ka pyaar %" pyaar
+if [[ $bandi == "daya bhabhi" ]];
+then
+  echo "Jetha is loyal"
+elif [[ $pyaar -ge 100 ]]
+  echo "Jetha is loyal"
+else
+  echo "Jetha is not loyal"
+fi
